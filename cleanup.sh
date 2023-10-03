6a71b09f9d727aa0defbb5c3ffb9a77c494b3c64
Anthony <aswingley@partsauthority.com>
2023-10-02 18:51:23 -0700

    1

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 podman container rm --all
 podman image rm localhost/ralph:latest
-podman build -t ralph src/.devcontainer --rm
+podman build --platform linux/arm64/v8 -t ralph src/.devcontainer --rm
 podman push localhost/ralph:latest tinkster42/ralph:latest
