29cd3b4bd644a96cd6cd37b2cceeb4c3e2e0f9a8
Anthony <aswingley@partsauthority.com>
2023-10-08 14:46:35 -0700

    1

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Start bluetooth
-service dbus start
-bluetoothd &
-
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
 source "/opt/ws_ralph/install/setup.bash" --
