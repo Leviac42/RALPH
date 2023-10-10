ac008b2af41b609eaed30317b22699a554693e75
Anthony <aswingley@partsauthority.com>
2023-10-09 18:26:17 -0700

    1

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-# source "/opt/ws_ralph/install/setup.bash" --
+source "/opt/ws_ralph/install/setup.bash" --
 # ros2 run motor_controller listener --
 exec "$@"
